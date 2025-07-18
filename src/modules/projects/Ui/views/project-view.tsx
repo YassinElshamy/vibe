2704c102d6f265dd7bebb97bd4c5b70d67dd6933
yassin-elshamy <elshamyyassin22@gmail.com>
2025-07-18 16:41:28 +0300

    19: bug fixes

--- src/modules/projects/Ui/views/project-view.tsx
+++ src/modules/projects/Ui/views/project-view.tsx
@@ -24,6 +24,7 @@ import { Button } from "@/components/ui/button";
 import { FileExplorer } from "@/components/file-explorer";
 import { UserControl } from "@/components/user-control";
 import { useAuth } from "@clerk/nextjs";
+import { ErrorBoundary } from "react-error-boundary";
 
 interface Props {
     projectId: string;
@@ -44,16 +45,20 @@ export const ProjectView = ({ projectId }: Props) => {
                     minSize = {20}
                     className="flex flex-col min-h-0"
                 >
-                    <Suspense fallback={<p>Loading project...</p>}>
-                        <ProjectHeader projectId={projectId} />
-                    </Suspense>
-                    <Suspense fallback={<div>Loading messages...</div>}>
-                        <MessagesContainer 
-                            projectId={projectId}
-                            activeFragment={activeFragment}
-                            setActiveFragment={setActiveFragment}
-                        />
-                    </Suspense>
+                    <ErrorBoundary fallback={<p>Project head error</p>}>
+                        <Suspense fallback={<p>Loading project...</p>}>
+                            <ProjectHeader projectId={projectId} />
+                        </Suspense>
+                    </ErrorBoundary>
+                    <ErrorBoundary fallback={<p>Messages container error</p>}>
+                        <Suspense fallback={<div>Loading messages...</div>}>
+                            <MessagesContainer 
+                                projectId={projectId}
+                                activeFragment={activeFragment}
+                                setActiveFragment={setActiveFragment}
+                            />
+                        </Suspense>
+                    </ErrorBoundary>
                 </ResizablePanel>
                 <ResizableHandle className="hover:bg-primary transition-colors" />
 
